b674e217c569f6cc2e2ed870c358a0feb1303b85
eduardo-granievicz <jogueinobigode@gmail.com>
2024-05-06 15:01:03 -0300

    refac: identação cadastroView

--- despesas/src/views/CadastroView.vue
+++ despesas/src/views/CadastroView.vue
@@ -1,16 +1,15 @@
 <template>
-    <Modal/>
+  <Modal />
 </template>
 
 <script>
-import Modal from '../components/Modal.vue'
+import Modal from "../components/Modal.vue";
 export default {
   name: "CadastroView",
   components: {
-    Modal
+    Modal,
   },
-  setup() {
-  }
+  setup() {},
 };
 </script>
 
