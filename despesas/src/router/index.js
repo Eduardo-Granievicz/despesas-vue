46e19f3dd3bf9cbd02f44c905b7fa8d8e0d52249
eduardo-granievicz <jogueinobigode@gmail.com>
2024-05-06 14:54:37 -0300

    feat: implementando rota do index

--- despesas/src/router/index.js
+++ despesas/src/router/index.js
@@ -1,5 +1,6 @@
 import CadastroView from '@/views/CadastroView.vue'
 import VisualizarView from '@/views/VisualizarView.vue'
+import IndexView from '@/views/IndexView.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 
@@ -8,6 +9,11 @@ const router = createRouter({
   routes: [
     {
       path: '/',
+      name: 'Index',
+      component: IndexView
+    },
+    {
+      path: '/cadastro',
       name: 'Cadastro',
       component: CadastroView
     },
