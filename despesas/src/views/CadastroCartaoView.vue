33249e36bb0447d635a1b850f7d81ebfe2aecd4f
eduardo-granievicz <jogueinobigode@gmail.com>
2024-05-06 17:07:59 -0300

    feat: criação da vue de cartão

---
+++ despesas/src/views/CadastroCartaoView.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <ModalCartao/>
+    </div>
+</template>
+
+<script>
+import ModalCartao from '@/components/ModalCartao.vue'
+
+export default {
+  name: "CadastroCartaoView",
+  components: {
+    ModalCartao
+  },
+  setup() {},
+};
+
+</script>
+
+<style>
+
+</style>
