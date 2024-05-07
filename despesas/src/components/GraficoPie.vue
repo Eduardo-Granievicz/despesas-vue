b5a546709b2cf81a89bdec48b7f85df8576f1694
eduardo-granievicz <jogueinobigode@gmail.com>
2024-05-07 16:23:41 -0300

    feat: criação componente grafico

---
+++ despesas/src/components/GraficoPie.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+      <apexchart type="pie" :options="chartOptions" :series="series" :labels="labels"></apexchart>
+    </div>
+  </template>
+  
+  <script>
+  import { ref } from 'vue';
+  import VueApexCharts from 'vue3-apexcharts';
+  
+  export default {
+    props: {
+      labels: {
+        type: Array,
+        required: false
+      },
+      series: Array, 
+    },
+    components: {
+      apexchart: VueApexCharts
+    },
+    setup(props) {
+      const chartOptions = ref({
+        chart: {
+          width: 380,
+          type: 'pie',
+        },
+        labels: props.labels
+      });
+  
+      return {
+        chartOptions
+      };
+    }
+  };
+  </script>
+  
